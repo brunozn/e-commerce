2a68a56579a187cd67254fdef7560a3d52c1c2de
brunozn <brunojndias@gmail.com>
2021-12-19 17:25:31 -0300

    products com admin, alguns produtos criados e migrates

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug", "created", "modified"]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "category",
+        "price",
+        "is_available",
+    ]
+    list_filter = ["is_available", "created", "modified"]
+    list_editable = ["price", "is_available"]
