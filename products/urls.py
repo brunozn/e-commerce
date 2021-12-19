eb467b8be229fea90967e42c5dff806a9cb280ad
brunozn <brunojndias@gmail.com>
2021-12-19 18:25:54 -0300

    resolved bug url category

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ from .views import ProductListView, ProductDetailView
 app_name="products"
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name="list"),
+    path("", ProductListView.as_view(), name="list"),
     path("<slug:slug>/", ProductDetailView.as_view(), name="detail"),
-    path("category/<slug:slug>/", ProductDetailView.as_view(), name="list_by_category"),
+    path("category/<slug:slug>/", ProductListView.as_view(), name="list_by_category"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from django.views.generic import DetailView, ListView
 
 from .models import Category, Product
